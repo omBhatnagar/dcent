67457006eee31f575de317d4bc961a93f19dee84
aman0802 <aman.khubani@gmail.com>
2022-09-23 03:17:32 +0530

    [AK] - updated sidebar items

--- utils/sidebar-items.js
+++ utils/sidebar-items.js
@@ -19,4 +19,16 @@ export const SidebarItems = [
     icon: <MdOutlineDashboard />,
     path: "/native",
   },
+  {
+    id: 3,
+    title: "Transactions",
+    icon: <MdOutlineDashboard />,
+    path: "/transactions",
+  },
+  {
+    id: 4,
+    title: "ERC20",
+    icon: <MdOutlineDashboard />,
+    path: "/balances/erc20",
+  },
 ];
