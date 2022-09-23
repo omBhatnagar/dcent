585af1e61a05f7061381b1efd8822566e4b6e056
omBhatnagar <omishb2001@gmail.com>
2022-09-23 20:57:43 +0530

    [OM] - Added background color to layout.

--- pages/nft.jsx
+++ pages/nft.jsx
@@ -8,47 +8,45 @@ const address = "0x91417d13cfDa2D1Ac6860f825ad964eA8E4343f8";
 const chain = EvmChain.RINKEBY;
 
 const Nfts = () => {
-  const [nfts, setNfts] = useState();
+	const [nfts, setNfts] = useState();
 
-  useEffect(() => {
-    (async () => {
-      await Moralis.start({
-        apiKey:
-          "VSrOoiuUp303vcqi8Od52Pg8kcZFhOKCBf3SfHP7eQYGl7GN2dfw4mbxoJCYvZAA",
-        // ...and any other configuration
-      });
+	useEffect(() => {
+		(async () => {
+			await Moralis.start({
+				apiKey: process.env.NEXT_PUBLIC_API_KEY,
+			});
 
-      const response = await Moralis.EvmApi.nft.getWalletNFTs({
-        address,
-        chain,
-      });
-      console.log(response.data.result);
-      setNfts(
-        response.data.result.map((item) => {
-          return JSON.parse(item.metadata);
-        })
-      );
-    })();
-  }, []);
+			const response = await Moralis.EvmApi.nft.getWalletNFTs({
+				address,
+				chain,
+			});
+			console.log(response.data.result);
+			setNfts(
+				response.data.result.map((item) => {
+					return JSON.parse(item.metadata);
+				}),
+			);
+		})();
+	}, []);
 
-  return (
-    <div className="flex flex-wrap justify-between items-center gap-y-12">
-      {nfts?.map((nft) => {
-        const image =
-          nft.image.substring(0, 4) == "ipfs"
-            ? "https://ipfs.io/" + nft.image
-            : nft.image;
-        return (
-          <NftCard
-            key={nft.hash}
-            name={nft.name}
-            description={nft.description}
-            image={image}
-          />
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className='flex flex-wrap justify-between items-center gap-y-12 bg-background-dark px-6'>
+			{nfts?.map((nft) => {
+				const image =
+					nft.image.substring(0, 4) == "ipfs"
+						? "https://ipfs.io/" + nft.image
+						: nft.image;
+				return (
+					<NftCard
+						key={nft.hash}
+						name={nft.name}
+						description={nft.description}
+						image={image}
+					/>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Nfts;
