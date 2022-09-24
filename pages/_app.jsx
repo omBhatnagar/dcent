c408aa8c299edd5e45c301abe7fa6a9a2c269ab8
AnadiSharma007 <anadis764@gmail.com>
2022-09-25 01:02:48 +0530

    changes app.js and removed unwanted components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,28 +2,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/shared/layout";
 import "../styles/globals.css";;
-import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { SessionProvider } from 'next-auth/react';
-
-const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
-
-const client = createClient({
-  provider,
-  webSocketProvider,
-  autoConnect: true,
-});
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-    <WagmiConfig client={client}>
-      <SessionProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      </SessionProvider>
-    </WagmiConfig>
     </ChakraProvider>
   );
 }
